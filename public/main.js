9b4c95d30329e93387db99855d1d362ebfc9c246
cloverdefa <39210648+cloverdefa@users.noreply.github.com>
2021-09-10 20:20:21 +0800

    修正main暱稱

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
 /* global Vue */
 (async () => {
   const gravatar = {
-    Samwumobile: { name: '魔王', avatar: 'bf73e08d8bc1db95b62f02d50f8a03e9' },
-    dk: { name: 'DK', avatar: '34624582cd585ba65e5b5368c84cb1a2' },
-    cloverdefa: {name: 'DAST', avatar: '9e202866b38b7255f282beb005576731'}
+    hirakujira: { name: 'DK', avatar: 'bf73e08d8bc1db95b62f02d50f8a03e9' },
+    Shawn_N: { name: 'DAST', avatar: '34624582cd585ba65e5b5368c84cb1a2' },
+    bill85101: {name: '魔王', avatar: '9e202866b38b7255f282beb005576731'}
   }
   const file = await fetch('public/saying.txt')
   const text = await file.text()
